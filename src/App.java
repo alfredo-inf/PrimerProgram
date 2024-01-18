e3af54928c2e4c27dc745386eb610f0fc0f6a701
Laboratorio 109 Pc-13 <lab-109-inf-13>
2024-01-17 21:05:05 -0400

    primer commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        String nombre = "Alfredo";
+        double monto = 475.50;
+        double resultado = monto * 1.15;
+
+        System.out.println ("Bienvenido "+nombre+". El total fue de "+resultado+".");
+
+    }
+}

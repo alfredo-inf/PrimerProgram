6b8255fa8d97b7f2b147c03d885ca118c09e5904
alfredo-inf <104326767+alfredo-inf@users.noreply.github.com>
2024-02-12 20:00:34 -0400

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,9 +1,13 @@
 public class App {
     public static void main(String[] args) {
+        //Ejemplo para presentar en clase
+
+        //Definicion e inicializacion de variables
         String nombre = "Alfredo";
         double monto = 475.50;
         double resultado = monto * 1.15;
 
+        //Impresion en pantalla
         System.out.println ("Bienvenido "+nombre+". El total fue de "+resultado+".");
 
     }
